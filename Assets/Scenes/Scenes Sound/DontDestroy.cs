c0dbf7c439c73e4ba0489cf0b5026d5a6e760950
Ahmed <ahmedmostafax288@gmail.com>
2021-01-13 07:20:42 +0200

    Complete sub menu (options) functionality

---
+++ Assets/Scenes/Scenes Sound/DontDestroy.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DontDestroy : MonoBehaviour
+{
+    void Awake(){
+        //This Step to prevent more than one sound object to be loaded if we go back to the Menu scene;
+        GameObject[] soundObjs = GameObject.FindGameObjectsWithTag("BG_MUSIC");
+        if(soundObjs.Length > 1)
+            Destroy(this.gameObject);
+
+        DontDestroyOnLoad(this.gameObject);
+    }
+}
